7b423919299574e4a1e02ebbf2ba8acb1063d75c
DecoderMagic <morrisseyluis@mailfence.com>
2024-12-28 08:46:20 -0500

    bootstrap updated

--- LifeRescripted/obj/Debug/net9.0/LifeRescripted.AssemblyInfo.cs
+++ LifeRescripted/obj/Debug/net9.0/LifeRescripted.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LifeRescripted")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ccfa43aa09923e20319fb4d773a39da553796d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c9b35ce19820e3155279b13b8636d6fb451ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LifeRescripted")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LifeRescripted")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
