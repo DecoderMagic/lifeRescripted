15fc49768817ace43fc539f10919ac858fb8f579
DecoderMagic <morrisseyluis@gmail.com>
2024-12-28 05:28:37 -0500

    Added view and controller for Journal and Photos

---
+++ LifeRescripted/Controllers/JournalController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeRescripted.Controllers
+{
+    public class JournalController : Controller
+    {
+        // GET: JournalController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ LifeRescripted/Controllers/PhotoController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeRescripted.Controllers
+{
+    public class PhotoController : Controller
+    {
+        // GET: PhotoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
