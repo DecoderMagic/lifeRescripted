8ccfa43aa09923e20319fb4d773a39da553796d4
DecoderMagic <morrisseyluis@mailfence.com>
2024-12-28 08:19:48 -0500

    small type

--- LifeRescripted/obj/Debug/net9.0/LifeRescripted.AssemblyInfo.cs
+++ LifeRescripted/obj/Debug/net9.0/LifeRescripted.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LifeRescripted")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15fc49768817ace43fc539f10919ac858fb8f579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c44f031ae6da2748be2eb8481ef93f0f004169f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LifeRescripted")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LifeRescripted")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
